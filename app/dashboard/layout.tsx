1ed4a6e9237e5fc5337dd2f0af180ae600446f62
Tsolo <tsolokhambule@gmail.com>
2025-09-22 16:13:04 +0200

    PPR using canary added

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,9 @@
 import SideNav from "@/app/ui/dashboard/sidenav"
+
+export const experimental = true
 export default function Layout({children}: {children: React.ReactNode}) {
+  
+
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
